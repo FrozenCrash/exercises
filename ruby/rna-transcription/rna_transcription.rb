17bf89d04d092db302ecb7fa38852f4095539d3b
frozencrash <zzcrash@gmail.com>
2016-08-02 01:42:49 +0300

    All code changed to RegExp

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,46 +1,7 @@
 class Complement
   def self.of_dna(strand)
-    val = ""
-    strand.each_char { |x| val << find_dna_complement(x) }
-    val
-  end
+    raise ArgumentError if strand.chars != (/^CGTA/)
 
-  def self.of_rna(strand)
-    val = ""
-    strand.each_char { |x| val << find_rna_complement(x) }
-    val
+    strand.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
   end
-
-  def self.find_dna_complement(nucleotides)
-    # case nucleotides
-    # when 'C'
-    #   'G'
-    # when 'G'
-    #   'C'
-    # when 'T'
-    #   'A'
-    # when 'A'
-    #   'U'
-    # when 'U'
-    #   ''
-    # when 'X'
-    #   ''
-    # end
-    { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U', 'U' => '', 'X' => ''}[nucleotides]
-  end
-
-  def self.find_rna_complement(nucleotides)
-    # case nucleotides
-    # when 'C'
-    #   'G'
-    # when 'G'
-    #   'C'
-    # when 'U'
-    #   'A'
-    # when 'A'
-    #   'T'
-    # end
-    {'C' => 'G', 'G' => 'C', 'U' => 'A', 'A' => 'T'}[nucleotides]
-  end
-
 end
