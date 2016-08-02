563f4704da1a65a4138a91cf6fcf17cfe2594df6
frozencrash <zzcrash@gmail.com>
2016-08-03 01:34:23 +0300

    Raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,21 +1,21 @@
 class Raindrops
-	CONST = {
-			3 => 'Pling',
-			5 => 'Plang',
-			7 => 'Plong'
-		}
-	def self.convert(val)
-		msg = ''
+  CONST = {
+      3 => 'Pling',
+      5 => 'Plang',
+      7 => 'Plong'
+    }
+  def self.convert(val)
+    msg = ''
 
-		CONST.each do |(num, str)|
-			msg << str if val % num == 0
-		end
+    CONST.each do |(num, str)|
+      msg << str if val % num == 0
+    end
 
-		if msg.empty?
-			val.to_s
-		else
-			msg
-		end
+    if msg.empty?
+      val.to_s
+    else
+      msg
+    end
 
-	end
+  end
 end
