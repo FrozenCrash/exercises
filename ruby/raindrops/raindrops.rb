0383a499c268eca83c7e62df252d29a60df6d657
frozencrash <zzcrash@gmail.com>
2016-08-02 23:31:39 +0300

    Ruby: Raindrops compleate

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,21 @@
+class Raindrops
+	CONST = {
+			3 => 'Pling',
+			5 => 'Plang',
+			7 => 'Plong'
+		}
+	def self.convert(val)
+		msg = ''
+
+		CONST.each do |(num, str)|
+			msg << str if val % num == 0
+		end
+
+		if msg.empty?
+			val.to_s
+		else
+			msg
+		end
+
+	end
+end
