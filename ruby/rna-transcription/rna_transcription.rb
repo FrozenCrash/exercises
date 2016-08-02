71766d1bae212c6a504755d680ac2fd7ce8d8f3c
frozencrash <zzcrash@gmail.com>
2016-08-02 19:30:48 +0300

    Finished task with RegExp

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,26 +1,11 @@
 class Complement
   def self.of_dna(strand)
-    # if strand.match(/[^BD-FH-SU-Z]/)
-    #   strand.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
-    # else 
-    #   ''
-    # end    
-    alphabet = {
-      'C' => 'G', 
-      'G' => 'C',
-      'T' => 'A',
-      'A' => 'U',
-      }
-    
     if strand.include?("X")
-    strand = ""
+      strand = ""
+    elsif strand.match(/[^BD-FH-SU-Z]/)
+      strand.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
+    else 
+      ''
     end
-
-    strand.split('').map do |x|
-      case strand
-        when !alphabet[x] then ''
-        else alphabet[x]
-      end
-    end.join('')
   end
 end
