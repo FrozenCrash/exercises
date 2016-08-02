cd277ac99301c6cd8aa32a0a78cd30340217c0e2
frozencrash <zzcrash@gmail.com>
2016-08-02 19:16:23 +0300

    Change code from regexp to ruby case

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,8 +1,23 @@
 class Complement
   def self.of_dna(strand)
-    if strand.match(/[^BD-FH-SU-Z]/)
-      strand.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
-    else ''
-    end    
+    # if strand.match(/[^BD-FH-SU-Z]/)
+    #   strand.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
+    # else 
+    #   ''
+    # end    
+    alphabet = {
+      'C' => 'G', 
+      'G' => 'C',
+      'T' => 'A',
+      'A' => 'U',
+      }
+
+    strand.split('').map do |x|
+      case strand
+        when !alphabet[x] then ''
+        when x.include?(nil) then ''
+        else alphabet[x]
+      end
+    end.join('')
   end
 end
