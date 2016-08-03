628de65216ed7001de1f18603b7ef79ae3c2839a
frozencrash <zzcrash@gmail.com>
2016-08-03 17:49:47 +0300

    Ruby: RNA Finished

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,13 +1,29 @@
 class Complement
-  def self.of_dna(strand)
-    a = /[BD-FH-SU-Z]/
-    b = /[^BD-FH-SU-Z]/
-    if strand.match(a)
-      strand = ""
-    elsif strand.match(b)
-      strand.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
-    else 
-      ''
+  def self.of_dna(strand) 
+    
+    return '' if strand.split('').any? { |letter| !['C', 'G', 'T', 'A'].include?(letter) } 
+    # return '' if strand.split('').any? { |x| x != ['C', 'G', 'T', 'A'] }
+
+    # strand.split('').map { |e|
+    # case e
+    # when 'C'
+    #   'G'
+    # when 'G'
+    #   'C' 
+    # when 'T' 
+    #   'A'
+    # when 'A' 
+    #   'U'
+    # else ''
+    # end
+    # }.join('')
+
+
+
+    if strand.each_char { |x| x == 'CGTA'}
+      strand.tr('CGTA', 'GCAU')
+    else
+      strand.clear
     end
   end
 end
